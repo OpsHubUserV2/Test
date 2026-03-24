3b467f567064a8adb1dc61f0320e7d44a9067701
OpsHubUserV2 <ayushi.patel@opshub.com>
2026-03-24 12:20:37 +0530

    Add files via upload
    Against ADO Id 298 for Issue #1

---
+++ Addition.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Calculating sum
+        int sum = num1 + num2;
+
+        // Output
+        System.out.println("Sum is: " + sum);
+
+        sc.close();
+    }
+}
